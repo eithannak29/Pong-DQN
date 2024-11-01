afdd1b2abd31a467f401e025a8c2b1ad3a47b0eb
eithan <eithan.nakache@epita.fr>
2024-11-01 03:13:10 +0100

    fix: training

--- src/utils.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-def preprocess_frame(frame):
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) 
-    frame = cv2.resize(frame, (84, 84)) 
-    frame = frame / 255.0 
-    return frame
